6676a929ad5c6d9d558293f15e9e0e9ab36da288
kiqiou <kuksare@gmail.com>
2025-11-10 14:21:22 +0300

    queue

--- src/queue.js
+++ src/queue.js
@@ -1,33 +1,32 @@
-import { NotImplementedError } from '../extensions/index.js';
+const { NotImplementedError } = require("../extensions/index.js");
 
-// import { ListNode } from '../extensions/list-node.js';
-
-/**
- * Implement the Queue with a given interface via linked list (use ListNode extension above).
- *
- * @example
- * const queue = new Queue();
- *
- * queue.enqueue(1); // adds the element to the queue
- * queue.enqueue(3); // adds the element to the queue
- * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
- * queue.getUnderlyingList() // returns { value: 3, next: null }
- */
-export default class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+module.exports = class Queue {
+  constructor() {
+    this.queueArray = [];
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  // Добавить элемент в конец очереди
+  enqueue(element) {
+    this.queueArray.push(element);
   }
 
+  // Удалить элемент из начала очереди
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.queueArray.shift();
   }
 
-}
+  // Посмотреть первый элемент (не удаляя его)
+  front() {
+    return this.queueArray.length > 0 ? this.queueArray[0] : undefined;
+  }
+
+  // Проверить, пуста ли очередь
+  isEmpty() {
+    return this.queueArray.length === 0;
+  }
+
+  // Получить количество элементов
+  size() {
+    return this.queueArray.length;
+  }
+};
